1b9aa97981cc361567031ebddd13b74a14bd12fb
ru1t3rl <lucaruiters@hotmail.nl>
2026-01-18 22:00:56 +0100

    Add a window style changer

---
+++ src/TileStyle.Core/Window/WindowStyleChanger.cs
@@ -0,0 +1,23 @@
+﻿namespace TileStyle.Windows;
+
+public partial class WindowStyleChanger
+{
+    internal void SetWindowStyle(IntPtr windowHandle, DWM_WINDOW_CORNER_PREFERENCE style)
+    {
+        DwmSetWindowAttribute(
+            windowHandle,
+            DWMWINDOWATTRIBUTE.DWMWA_WINDOW_CORNER_PREFERENCE,
+            ref style,
+            sizeof(uint));
+    }
+
+    public void SwitchToFloatingStyle(IntPtr windowHandle)
+    {
+        SetWindowStyle(windowHandle, DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_ROUND);
+    }
+
+    public void SwitchToFullHeightStyle(IntPtr windowHandle)
+    {
+        SetWindowStyle(windowHandle, DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_DONOTROUND);
+    }
+}
---
+++ src/TileStyle.Core/Window/WindowStyleChanger.extern.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+namespace TileStyle.Windows;
+
+public partial class WindowStyleChanger
+{
+    internal  enum DWMWINDOWATTRIBUTE : uint
+    {
+        DWMWA_WINDOW_CORNER_PREFERENCE = 33
+    }
+
+    internal  enum DWM_WINDOW_CORNER_PREFERENCE : uint
+    {
+        DWMWCP_DEFAULT    = 0,  // System default
+        DWMWCP_DONOTROUND = 1,  // Never round
+        DWMWCP_ROUND      = 2,  // Round if appropriate
+        DWMWCP_ROUNDSMALL = 3   // Small rounding
+    }
+
+    
+    [DllImport("dwmapi.dll")]
+    private extern static int DwmSetWindowAttribute(
+        IntPtr hwnd,
+        DWMWINDOWATTRIBUTE attribute,
+        ref DWM_WINDOW_CORNER_PREFERENCE pvAttribute,
+        uint cbAttribute);
+}
